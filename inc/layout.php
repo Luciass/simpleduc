ff5c7b85fc79b9e70ba39271ddea2e9090af8b3a
thomas <thomas.delos@epsi.fr>
2023-03-23 11:39:40 +0100

    login

--- inc/layout.php
+++ inc/layout.php
@@ -15,6 +15,7 @@
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link href="css/StylesIndex.css" rel="stylesheet">
+<link rel="stylesheet" href="/simpleduc/css/Connexion.css">
 <script src="https://kit.fontawesome.com/222489915d.js" crossorigin="anonymous"></script>
 </head>
 <body>
